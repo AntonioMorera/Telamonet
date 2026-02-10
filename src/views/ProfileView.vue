43784e8a0bf2afe5edefd08c1c141031e67373fa
MedhoMs <danielbucaloiu@gmail.com>
2026-02-10 10:07:27 +0000

    verified tick

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -11,8 +11,8 @@ const { t } = useTranslations()
 const usuario = {
    nombre: 'TelamoNet',
    usuarioNombre: '@telamonet',
-   seguidores: 100,
-   siguiendo: 75,
+   seguidores: '1M',
+   siguiendo: '3',
    bannerUrl: 'https://estaticos-cdn.prensaiberica.es/clip/3bffd319-f839-4e57-9ccb-b95ec474f104_source-aspect-ratio_default_0.jpg',
    iconoUrl: '../assets/logo/logoTelamon.png'
 }
@@ -51,9 +51,10 @@ const usuario = {
 
             <!-- Información del perfil -->
             <div class="px-5 pb-2.5 -mt-2">
-                <h2 class="nombre text-2xl font-bold m-0 text-[#e7e9ea]">
-                {{ usuario.nombre }}
-                </h2>
+                <div class="flex items-center gap-1">
+                    <h2 class="nombre text-2xl font-bold m-0 text-[#e7e9ea]">{{ usuario.nombre }}</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#179cf0" class="icon icon-tabler icons-tabler-filled icon-tabler-rosette-discount-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.01 2.011a3.2 3.2 0 0 1 2.113 .797l.154 .145l.698 .698a1.2 1.2 0 0 0 .71 .341l.135 .008h1a3.2 3.2 0 0 1 3.195 3.018l.005 .182v1c0 .27 .092 .533 .258 .743l.09 .1l.697 .698a3.2 3.2 0 0 1 .147 4.382l-.145 .154l-.698 .698a1.2 1.2 0 0 0 -.341 .71l-.008 .135v1a3.2 3.2 0 0 1 -3.018 3.195l-.182 .005h-1a1.2 1.2 0 0 0 -.743 .258l-.1 .09l-.698 .697a3.2 3.2 0 0 1 -4.382 .147l-.154 -.145l-.698 -.698a1.2 1.2 0 0 0 -.71 -.341l-.135 -.008h-1a3.2 3.2 0 0 1 -3.195 -3.018l-.005 -.182v-1a1.2 1.2 0 0 0 -.258 -.743l-.09 -.1l-.697 -.698a3.2 3.2 0 0 1 -.147 -4.382l.145 -.154l.698 -.698a1.2 1.2 0 0 0 .341 -.71l.008 -.135v-1l.005 -.182a3.2 3.2 0 0 1 3.013 -3.013l.182 -.005h1a1.2 1.2 0 0 0 .743 -.258l.1 -.09l.698 -.697a3.2 3.2 0 0 1 2.269 -.944zm3.697 7.282a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" /></svg>
+                </div>
                 <p class="nombre-usuario text-[#8b98a5] text-base my-0.5 mx-0">
                 {{ usuario.usuarioNombre }}
                 </p>
